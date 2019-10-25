b162db9afa10d99abfb02473b95a9a74fb9587c6
Your Name <you@example.com>
2019-10-26 03:33:54 +0530

    first commit

---
+++ administrator/logs/1.error.php
@@ -0,0 +1,11 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2019-10-10 18:19:03 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority clientip	category	message
+2019-10-10T18:19:03+00:00	INFO 127.0.0.1	joomlafailure	Username and password do not match or you do not have an account yet.
+2019-10-10T18:19:09+00:00	INFO 127.0.0.1	joomlafailure	Username and password do not match or you do not have an account yet.
+2019-10-10T18:19:18+00:00	INFO 127.0.0.1	joomlafailure	Username and password do not match or you do not have an account yet.
+2019-10-10T18:19:34+00:00	INFO 127.0.0.1	joomlafailure	Username and password do not match or you do not have an account yet.
+2019-10-10T18:20:56+00:00	INFO 127.0.0.1	joomlafailure	Username and password do not match or you do not have an account yet.
